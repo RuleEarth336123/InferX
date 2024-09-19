1e8fd1eeaa3e522f2b9484bf0440ed6c2ea5c227
luyunchun@2023163.com <luyunchun2023@163.com>
2024-09-19 13:12:09 +0800

    update llama2class

--- src/models/llama2.cpp
+++ src/models/llama2.cpp
@@ -348,21 +348,174 @@ void model::Llama2Model::create_param_layers()
 
 void model::Llama2Model::create_nonparam_layers()
 {
-    
-
+    CHECK(llama_layers_ != nullptr);
+    llama_layers_->rope_layer_ = std::make_shared<op::RopeLayer>(
+        device_type_, config_->dim_, config_->kv_dim_, config_->head_size_);
+    llama_layers_->mha_layer_ = std::make_shared<op::MultiHeadAttention>(
+        device_type_, 0, config_->kv_mul_, config_->kv_dim_, config_->seq_len_, config_->head_num_,config_->head_size_);
+    llama_layers_->add_layer_ = std::make_shared<op::VecAddLayer>(device_type_);
+    llama_layers_->swiglu_layer_ = std::make_shared<op::SwigluLayer>(device_type_, config_->hidden_dim_);
 
 }
 
 void model::Llama2Model::create_param_quant_layers()
 {
+    CHECK(is_quant_model_);
+    CHECK(llama_layers_ != nullptr);
+
+    // 初始化matmul层，pos用于跟踪权重数据的位置
+    size_t pos = 0;
+    
+    //创建查询矩阵(dim * dim)
+    for(int32_t i = 0;i < config_->layer_num_; i++){
+        auto wq = std::make_shared<op::MatmulLayer>(
+            device_type_,config_->dim_,config_->dim_,true
+        );
+        wq->set_group_size(group_size_);
+        wq->set_weight(
+            0, {config_->dim_, config_->dim_}, this->raw_model_data_->weight(pos), base::DeviceType::kDeviceCPU
+        );
+        llama_layers_->wq_layers_.push_back(wq);
+        pos = pos + config_->dim_ * config_->dim_ + wq->get_scale_num() * sizeof(float);
+    }
+
+    //创建键矩阵(kx_dim * dim)
+    for(int32_t i = 0;i < config_->layer_num_; i++){
+        auto wk = std::make_shared<op::MatmulLayer>(
+            device_type_,config_->kv_dim_,config_->dim_,true
+        );
+        wk->set_group_size(group_size_);
+        wk->set_weight(
+            0, {config_->kv_dim_, config_->dim_}, this->raw_model_data_->weight(pos), base::DeviceType::kDeviceCPU
+        );
+        llama_layers_->wk_layers_.push_back(wk);
+        pos = pos + config_->kv_dim_ * config_->dim_ + wk->get_scale_num() * sizeof(float);
+    } 
+
+    //创建值矩阵(kx_dim * dim)
+    for(int32_t i = 0;i < config_->layer_num_; i++){
+        auto wv = std::make_shared<op::MatmulLayer>(
+            device_type_,config_->kv_dim_,config_->dim_,true
+        );
+        wv->set_group_size(group_size_);
+        wv->set_weight(
+            0, {config_->kv_dim_, config_->dim_}, this->raw_model_data_->weight(pos), base::DeviceType::kDeviceCPU
+        );
+        llama_layers_->wv_layers_.push_back(wv);
+        pos = pos + config_->kv_dim_ * config_->dim_ + wv->get_scale_num() * sizeof(float);
+    } 
+
+    //创建输出矩阵(dim * dim)
+    for(int32_t i = 0;i < config_->layer_num_; i++){
+        auto wo = std::make_shared<op::MatmulLayer>(
+            device_type_,config_->dim_,config_->dim_,true
+        );
+        wo->set_group_size(group_size_);
+        wo->set_weight(
+            0, {config_->dim_, config_->dim_}, this->raw_model_data_->weight(pos), base::DeviceType::kDeviceCPU
+        );
+        llama_layers_->wo_layers_.push_back(wo);
+        pos = pos + config_->dim_ * config_->dim_ + wo->get_scale_num() * sizeof(float);
+    } 
+
+
+    //w1 layer
+    for(int32_t i = 0; i < config_->layer_num_; i++){
+        auto w1 = std::make_shared<op::MatmulLayer>(device_type_,config_->hidden_dim_,config_->dim_,true);
+        w1->set_weight(
+            0,{config_->hidden_dim_,config_->dim_},this->raw_model_data_->weight(pos),base::DeviceType::kDeviceCPU);
+        llama_layers_->w1_layers.push_back(w1);
+        pos += config_->dim_ * config_->hidden_dim_;
+    }
+
+    //w2 layer
+    for(int32_t i = 0; i < config_->layer_num_; i++){
+        auto w2 = std::make_shared<op::MatmulLayer>(device_type_,config_->hidden_dim_,config_->dim_,true);
+        w2->set_weight(
+            0,{config_->hidden_dim_,config_->dim_},this->raw_model_data_->weight(pos),base::DeviceType::kDeviceCPU);
+        llama_layers_->w2_layers.push_back(w2);
+        pos += config_->dim_ * config_->hidden_dim_;
+    }
+
+    //w3 layer
+    for(int32_t i = 0; i < config_->layer_num_; i++){
+        auto w3 = std::make_shared<op::MatmulLayer>(device_type_,config_->hidden_dim_,config_->dim_,true);
+        w3->set_weight(
+            0,{config_->hidden_dim_,config_->dim_},this->raw_model_data_->weight(pos),base::DeviceType::kDeviceCPU);
+        llama_layers_->w3_layers.push_back(w3);
+        pos += config_->dim_ * config_->hidden_dim_;
+    }
+
+
+
+    // 创建分类层（classification layer）
+    llama_layers_->cls_layer_ = std::make_shared<op::MatmulLayer>(device_type_,config_->vocab_size_,config_->dim_,true);
+    llama_layers_->cls_layer_->set_group_size(group_size_);
+    if(config_->is_shared_weight_){
+        llama_layers_->cls_layer_->set_weight(
+            0,{config_->vocab_size_, config_->dim_},this->raw_model_data_->weight(0), base::DeviceType::kDeviceCPU);
+    }else{
+        llama_layers_->cls_layer_->set_weight(
+            0,{config_->vocab_size_, config_->dim_},this->raw_model_data_->weight(pos), base::DeviceType::kDeviceCPU);
+        pos = pos + config_->vocab_size_ * config_->dim_ + llama_layers_->cls_layer_->get_scale_num() * sizeof(float);
+    }
+
+    //embeding层
+    float* weight_ptr = (float*)raw_model_data_->weight(pos);
+    llama_layers_->embeding_layer_ = std::make_shared<op::EmbeddingLayer>(
+        device_type_,config_->dim_,config_->seq_len_,std::abs(config_->vocab_size_)
+    );
+    llama_layers_->embedding_layer_->set_weight(
+        0, {std::abs(config_->vocab_size_), config_->dim_}, weight_ptr,base::DeviceType::kDeviceCPU
+    );
+
+    weight_ptr += config_->vocab_size_ * config_->dim_;
+
+    // rmsnorm attention attention,ffn,final
+    for(int32_t i = 0;i < 2*config_->layer_num_ + 1;i++){
+        std::shared_ptr<op::RmsnormLayer> rms_norm_layer = std::make_shared<op::RmsnormLayer>(device_type_,config_->dim_);
+        rms_norm_layer->set_weight(0, {config_->dim_}, weight_ptr, base::DeviceType::kDeviceCPU);
+        llama_layers_->rmsnorm_layers_.push_back(rms_norm_layer);
+        weight_ptr += config_->dim_;
+    } 
 }
 
 void model::Llama2Model::attention_mha(int32_t layer_idx, const tensor::Tensor &pos_tensor) const
 {
+    using namespace tensor;
+    CHECK(llama_layers_ != nullptr);
+
+    Tensor key_cache = get_buffer(ModelBufferType::kKeyCache);
+    Tensor val_cache = get_buffer(ModelBufferType::kValueCache);
+    Tensor mha_output = get_buffer(ModelBufferType::kOutputMHA);
+    Tensor score_storage = get_buffer(ModelBufferType::kScoreStorage);
+    Tensor query = get_buffer(ModelBufferType::kQuery);
+
+    const auto& mha_layer = llama_layers_->mha_layer_;
+    CHECK_NE(mha_layer, nullptr) << "The multi head attention layer is null pointer.";
+
+    int pos = pos_tensor.index<int32_t>(0);
+    std::dynamic_pointer_cast<op::MultiHeadAttention>(mha_layer)->set_pos(pos);
+    std::dynamic_pointer_cast<op::MultiHeadAttention>(mha_layer)->set_layer_idx(layer_idx);
+    STATUS_CHECK(mha_layer->forward(query, score_storage, key_cache, val_cache, mha_output));
+
+    Tensor attn_output = get_buffer(ModelBufferType::kAttnOutput);
+    const auto& wo_layer = llama_layers_->wo_layers_.at(layer_idx);
+    CHECK_NE(wo_layer, nullptr) << "The weight output layer is null pointer.";
+    STATUS_CHECK(wo_layer->forward(mha_output, attn_output));
+
 }
 
 void model::Llama2Model::attention_rms(int32_t layer_idx, const tensor::Tensor &input) const
 {
+    CHECK(llama_layers_ != nullptr);
+    tensor::Tensor rmsnorm_output = get_buffer(ModelBufferType::kOutputRMSNorm);
+    std::shared_ptr<op::Layer> rmsnorm_layer = llama_layers_->rmsnorm_layers_.at(layer_idx);
+    if(!rmsnorm_layer){
+        std::cerr << "The attention rmsnorm layer is a null pointer in the llama2 model" << std::endl;
+    }
+    STATUS_CHECK(rmsnorm_layer->forward(input, rmsnorm_output));
+
 }
 
 void model::Llama2Model::feed_forward(int32_t layer_idx, const tensor::Tensor &input) const
@@ -371,6 +524,30 @@ void model::Llama2Model::feed_forward(int32_t layer_idx, const tensor::Tensor &i
 
 void model::Llama2Model::attention_qkv(int32_t layer_idx, const tensor::Tensor &pos_tensor) const
 {
+    CHECK(llama_layers_ != nullptr);
+    tensor::Tensor query = this->get_buffer(ModelBufferType::kQuery);
+    int32_t pos = pos_tensor.index<int32_t>(0);
+
+    // wq wk wv @ input
+    const auto& [key, val] = slice_kv_cache(layer_idx, pos);
+    // query
+    const auto& query_layer = llama_layers_->wq_layers_.at(layer_idx);
+    CHECK_NE(query_layer, nullptr) << "The query layer in the attention block is null pointer.";
+    auto rmsnorm_output = get_buffer(ModelBufferType::kOutputRMSNorm);
+    STATUS_CHECK(query_layer->forward(rmsnorm_output, query));
+    // key
+    const auto& key_layer = llama_layers_->wk_layers_.at(layer_idx);
+    CHECK_NE(key_layer, nullptr) << "The key layer in the attention block is null pointer.";
+    STATUS_CHECK(key_layer->forward(rmsnorm_output, key));
+    // value
+    const auto& value_layer = llama_layers_->wv_layers_.at(layer_idx);
+    CHECK_NE(value_layer, nullptr) << "The value layer in the attention block is null pointer.";
+    STATUS_CHECK(value_layer->forward(rmsnorm_output, val));
+    // rope
+    CHECK_NE(llama_layers_->rope_layer_, nullptr)
+        << "The RoPE layer in the attention block is null pointer.";
+    STATUS_CHECK(llama_layers_->rope_layer_->forward(query, key, pos_tensor, tensor::Tensor{}));
+
 }
 
 void model::Llama2Model::cls_logits(const tensor::Tensor &input) const
